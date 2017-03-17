063a4a6bbbce8830c3dafa8b3b4945e6ca7f445b
Konnor King <konnor.king@gmail.com>
2017-03-17 13:55:35 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>alpha</title>
+  <!--  No href external links to css or js-->
+   <link rel="stylesheet" href="css/custom.css">
+	    <style>
+
+img {
+    max-width: 100%;
+    height: auto;
+}
+
+img:hover {
+     opacity: 0.5;
+}
+
+		}
+</style>
+
+</head>
+<body>
+<center>
+	<img src = "img/alpha.png" align = "left" height = "40px">
+<img src = "img/Custom.png" align = "right" height = "40px">
+<br />
+<a href = "html/" target = "_blank">
+	<img src = "img/buttons/html.png">
+</a>
+<a href = "php/" target = "_blank">
+	<img src = "img/buttons/php.png">
+</a>
+<a href = "networking/" target = "_blank">
+	<img src = "img/buttons/networking.png">
+</a>
+<a href = "downloads/" target = "_blank">
+	<img src = "img/buttons/downloads.png"
+</a>
+<a href = "http://10.183.2.1:8080" target = "_blank">
+	<img src = "img/buttons/8080.png"
+</a>
+<a href = "qnb/pma/" target = "_blank">
+	<img src = "img/buttons/pma.png"
+</a>
+<a href = "logs/" target = "_blank">
+	<img src = "img/buttons/logs.png"
+</a>
+<a href = "code/" target = "_blank">
+	<img src = "img/buttons/code.png"
+</a>
+<a href = "contact.html" target = "_blank">
+	<img src = "img/buttons/contact.png"
+</a>
+<a href = "https://sites.google.com/a/tritechsc.org/konnork/" target = "_blank">
+	<img src = "img/buttons/google.jpg" align = "bottom" width='160px' height='160px'>
+</a>
+<pre><font color="black">
+Welcome to alpha.ioe
+<p><font color="black"><background color = "red">Your browser is now more <font color="red">Lit</p>
+<div class="three">
+
+<img src = "img/buttons/superbad.png" height = "200px" width = "200px" align = "center">
+<p><font color="black"><font size="3.5"<p>
+</div>
+</center>
+</pre>
+<h1><font color="black"><font size="3.5">I'm too lazy to make a very detailed website so here's an iframe of Kallen's</h1>
+<iframe id="if-location" name="if" arc="into.html" scrolling="yes" frameborder="7"
+	<iframe src = "http://10.183.2.2/">
+</iframe>
+
+
+</body>
+</html>
